51b9497fded6db9c2dc5f03df6b59b42dcf9cc3a
Christopher Diggins <cdiggins@gmail.com>
2018-10-05 15:38:13 -0400

    Simplified IGeometry interface: it is all PolyMeshes now

--- Geometry.cs
+++ Geometry.cs
@@ -5,115 +5,115 @@ using System.Linq;
 
 namespace Ara3D
 {
-    public interface IElement<T> : IArray<int> where T : IElement<T>
+    public interface IElement : IArray<int> 
     {
-        IGeometry<T> Geometry { get; }
+        IGeometry Geometry { get; }
     }
 
     // An element is a generaliztion of a face / point / line /
-    public struct Element<T> where T : IElement<T>
+    public struct Element<T> where T : IElement
     {
         public int Index;
-        public IGeometry<T> Geometry;
+        public IGeometry Geometry;
     }
 
-    public interface IGeometry<T> : IMemoizer where T : IElement<T>
+    public interface IGeometry : IMemoizer 
     {
         IArray<Vector3> Vertices { get; }
-        IArray<T> Elements { get; }
+        IArray<IElement> Elements { get; }
     }
 
-    public struct PolyFace : IElement<PolyFace>
+    public struct PolyFace : IElement
     {
-        public IGeometry<PolyFace> Geometry { get; }
-        public PolyFace(IGeometry<PolyFace> g, IArray<int> indices)
+        public IGeometry Geometry { get; }
+        public PolyFace(IGeometry g, IArray<int> indices)
         {
             Geometry = g;
             Indices = indices;
         }
-        public int Count { get { return Indices.Count; } }
+        public int Count => Indices.Count;
         public IArray<int> Indices { get; }
         public int this[int n] { get { return Indices[n]; } }
     }
 
-    public struct QuadFace : IElement<QuadFace>
+    public struct QuadFace : IElement
     {
-        public IGeometry<QuadFace> Geometry { get; }
-        public QuadFace(IGeometry<QuadFace> g, int a, int b, int c, int d)
+        public IGeometry Geometry { get; }
+        public QuadFace(IGeometry g, int a, int b, int c, int d)
         {
             Geometry = g;
             A = a; B = b; C = c; D = d;
         }
         public int A, B, C, D;
-        public int Count { get { return 4; } }
+        public int Count => 4;
         public int this[int n] { get { return n == 0 ? A : n == 1 ? B : n == 2 ? C : D; } }
     }
 
-    public struct TriFace : IElement<TriFace>
+    public struct TriFace : IElement
     {
-        public TriFace(IGeometry<TriFace> g, int a, int b, int c)
+        public TriFace(IGeometry g, int a, int b, int c)
         {
             Geometry = g;
             A = a; B = b; C = c;
         }
         public int A, B, C;
-        public IGeometry<TriFace> Geometry { get; }
-        public int Count { get { return 3; } }
+        public IGeometry Geometry { get; }
+        public int Count => 3;
         public int this[int n] { get { return n == 0 ? A : n == 1 ? B : C; } }
     }
 
-    public struct Line : IElement<Line>
+    public struct Line : IElement
     {
-        public IGeometry<Line> Geometry { get; }
+        public IGeometry Geometry { get; }
         public int A, B;
-        public int Count { get { return 2; } }
+        public int Count => 2;
         public int this[int n] { get { return n == 0 ? A : B; } }
     }
 
-    public struct Point : IElement<Point>
+    public struct Point : IElement
     {
-        public IGeometry<Point> Geometry { get; }
-        public int Count { get { return 1; } }
+        public IGeometry Geometry { get; }
+        public int Count => 1;
         public int Index { get; }
         public int this[int n] { get { return Index; } }
     }
 
-    public struct SphereElement : IElement<SphereElement>
+    public struct SphereElement : IElement
     {
-        public IGeometry<SphereElement> Geometry { get; }
-        public int Count { get { return 1; } }
+        public IGeometry Geometry { get; }
+        public int Count => 1;
         public int Index { get; }
         public int this[int n] { get { return Index; } }
     }
 
-    public struct BoxElement : IElement<BoxElement>
+    public struct BoxElement : IElement
     {
-        public IGeometry<BoxElement> Geometry { get; }
-        public int Count { get { return 1; } }
+        public IGeometry Geometry { get; }
+        public int Count => 1;
         public int Index { get; }
         public int this[int n] { get { return Index; } }
     }
 
-    public struct Edge<T> where T : IElement<T>
+    public struct Edge 
     {
-        public Edge(T e, int i) { Element = e; Index = i; }
-        public T Element { get; }
+        public Edge(IElement e, int i) { Element = e; Index = i; }
+        public IElement Element { get; }
         public int Index { get; }        
     }
 
-    public struct Corner<T> where T : IElement<T>
+    public struct Corner
     {
-        public T Element { get; }
+        public IElement Element { get; }
         public int Index { get; }
     }
 
-    public struct Vertex<T> where T : IElement<T>
+    public struct Vertex
     {
-        public IGeometry<T> Geometry { get; }
+        public IGeometry Geometry { get; }
         public int Index { get; }
     }
 
-    public class BaseMesh: IGeometry<PolyFace>
+    public class BaseMesh: IGeometry
     {
         public BaseMesh(IArray<Vector3> vertices, IArray<int> indices, IArray<int> faceIndices)
         {
@@ -123,9 +123,9 @@ namespace Ara3D
         }
         public IArray<int> Indices { get; }
         public IArray<int> FaceIndices { get; }
-        public IArray<int> FaceCounts { get { return FaceIndices.Append(Indices.Count).AdjacentDifferences(); } }
+        public IArray<int> FaceCounts => FaceIndices.Append(Indices.Count).AdjacentDifferences();
         public IArray<Vector3> Vertices { get; }
-        public IArray<PolyFace> Elements { get { return FaceIndices.Zip(FaceCounts, (i, c) => this.MakePolyFace(Indices.Subarray(i, c))); } }
+        public IArray<IElement> Elements => FaceIndices.Zip(FaceCounts, (i, c) => this.PolyFace(Indices.Subarray(i, c)) as IElement);
         public ConcurrentDictionary<object, object> Cache { get; } = new ConcurrentDictionary<object, object>();
     }
 
@@ -136,22 +136,22 @@ namespace Ara3D
         { }
     }
 
-    public class TriMesh : BaseMesh, IGeometry<TriFace> 
+    public class TriMesh : BaseMesh, IGeometry 
     {
         public TriMesh(IArray<Vector3> vertices, IArray<int> indices)
             : base(vertices, indices, indices.Indices().Stride(3))
         {
-            Elements = (Indices.Count / 3).Select(i => this.MakeTriFace(Indices[i * 3], Indices[i * 3 + 1], Indices[i * 3 + 2]));
+            Elements = (Indices.Count / 3).Select(i => this.TriFace(Indices[i * 3], Indices[i * 3 + 1], Indices[i * 3 + 2]));
         }
         public new IArray<TriFace> Elements { get; }
     }
 
-    public class QuadMesh : BaseMesh, IGeometry<QuadFace>
+    public class QuadMesh : BaseMesh, IGeometry
     {
         public QuadMesh(IArray<Vector3> vertices, IArray<int> indices)
-            : base(vertices, indices, indices.Indices().Stride(3))
+            : base(vertices, indices, indices.Indices().Stride(4))
         {
-            Elements = (Indices.Count / 4).Select(i => this.MakeQuadFace(Indices[i * 4], Indices[i * 4 + 1], Indices[i * 4 + 2], Indices[i * 4 + 3]));
+            Elements = (Indices.Count / 4).Select(i => this.QuadFace(Indices[i * 4], Indices[i * 4 + 1], Indices[i * 4 + 2], Indices[i * 4 + 3]));
         }
         public new IArray<QuadFace> Elements { get; }
     }
@@ -198,63 +198,42 @@ namespace Ara3D
 
     public static class Geometry
     {
-        public static IArray<IArray<Edge<T>>> Edges<T>(this IGeometry<T> self) where T: IElement<T>
+        public static IArray<IArray<Edge>> Edges(this IGeometry self)
         {
             return self.Elements.Select(Edges);
         }
 
-        public static IArray<Edge<T>> Edges<T>(this T self) where T : IElement<T>
+        public static IArray<Edge> Edges(this IElement self) 
         {
-            return self.Select(i => new Edge<T>(self, i));
+            return self.Select(i => new Edge(self, i));
         }
 
-        public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> self, U init, Func<U, T, U> f) 
-        {
-            foreach (var x in self)
-                yield return init = f(init, x);
-        }
-
-        public static IEnumerable<T> Accumulate<T>(this IEnumerable<T> self, Func<T, T, T> f)
-        {
-            return self.Accumulate(default(T), f);
-        }
-
-        public static IEnumerable<double> PartialSums(this IEnumerable<double> self)
-        {
-            return self.Accumulate((x, y) => x + y);
-        }
-
-        public static IEnumerable<int> PartialSums(this IEnumerable<int> self)
-        {
-            return self.Accumulate((x, y) => x + y);
-        }
-
-        public static IEnumerable<int> EnumerateIndices<T>(this IGeometry<T> g) where T: IElement<T>
+        public static IEnumerable<int> EnumerateIndices(this IGeometry g)
         {
             return g.Elements.ToEnumerable().SelectMany(e => e.ToEnumerable());
         }
 
-        public static IArray<int> UncachedIndices<T>(this IGeometry<T> g) where T: IElement<T>
+        public static IArray<int> UncachedIndices(this IGeometry g)
         {
             return g.EnumerateIndices().ToIArray();
         }
 
-        public static IArray<int> Indices<T>(this IGeometry<T> g) where T: IElement<T>
+        public static IArray<int> Indices(this IGeometry g)
         {
             return g.Memoize(g.UncachedIndices);
         }
 
-        public static TriFace MakeTriFace(this IGeometry<TriFace> self, int a, int b, int c)
+        public static TriFace TriFace(this IGeometry self, int a, int b, int c)
         {
             return new TriFace(self, a, b, c);
         }
 
-        public static QuadFace MakeQuadFace(this IGeometry<QuadFace> self, int a, int b, int c, int d)
+        public static QuadFace QuadFace(this IGeometry self, int a, int b, int c, int d)
         {
             return new QuadFace(self, a, b, c, d);
         }
 
-        public static PolyFace MakePolyFace(this IGeometry<PolyFace> self, IArray<int> indices)
+        public static PolyFace PolyFace(this IGeometry self, IArray<int> indices)
         {
             return new PolyFace(self, indices);
         }
--- Scene.cs
+++ Scene.cs
@@ -2,9 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Ara3D
+namespace Ara3D.Scenes
 {
-    public class Scene
+    public class Entity
     {
+        public string Name;
+        public Guid Id;
+    }
+
+    public class Material : Entity
+    {
+        public Vector4? Diffuse { get; }
+        public Vector4? Specular { get; }
+        public Vector4? Ambient { get; }
+        public float Opacity { get; } = 1.0f;
+        public float Transparency { get { return 1.0f - Opacity; } }
+        public float? OpticalDenisty { get; }
+    }
+
+    public class Scene : Entity
+    {
+        IArray<Node> Nodes { get; }
+    }
+
+    public class Node : Entity
+    {
+        public Node Parent { get; }
+        public Matrix4x4 WorldTransform { get; }
+        public GeometricObject Geometry { get; }
+    }
+
+    public class GeometricObject : Entity
+    {
+        public IArray<Material> Materials { get; }
+        public IArray<IGeometry> Geometries { get; }
     }
 }
