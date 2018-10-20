aa73879d3afd56c5f2f2ce3fc8457fa2a5c7cbaa
Christopher Diggins <cdiggins@gmail.com>
2018-10-20 11:08:46 -0400

    Improving geometry algorithms

--- Geometry.cs
+++ Geometry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Ara3D
@@ -94,11 +95,13 @@ namespace Ara3D
         public int this[int n] { get { return Index; } }
     }
 
-    public struct Edge 
+    public struct Edge : IArray<Vector3>
     {
         public Edge(IElement e, int i) { Element = e; Index = i; }
         public IElement Element { get; }
-        public int Index { get; }        
+        public int Index { get; }       
+        public int Count { get { return 2; } }
+        public Vector3 this[int n] { get { return Element.Points().ElementAtModulo(Index + n); } }
     }
 
     public struct Corner
@@ -136,7 +139,7 @@ namespace Ara3D
         { }
     }
 
-    public class TriMesh : BaseMesh, IGeometry 
+    public class TriMesh : BaseMesh
     {
         public TriMesh(IArray<Vector3> vertices, IArray<int> indices)
             : base(vertices, indices, indices.Indices().Stride(3))
@@ -146,7 +149,7 @@ namespace Ara3D
         public new IArray<TriFace> Elements { get; }
     }
 
-    public class QuadMesh : BaseMesh, IGeometry
+    public class QuadMesh : BaseMesh
     {
         public QuadMesh(IArray<Vector3> vertices, IArray<int> indices)
             : base(vertices, indices, indices.Indices().Stride(4))
@@ -196,8 +199,24 @@ namespace Ara3D
         }
     }
 
+    public static class ArrayHelpers
+    {
+        public static Vector3 Sum(this IArray<Vector3> self)
+        {
+            return self.Aggregate(Vector3.Zero, (a, b) => a + b);
+        }
+
+        public static Vector3 Average(this IArray<Vector3> self)
+        {
+            return self.Sum() / self.Count;
+        }
+    }
+
     public static class Geometry
     {
+        // Epsilon is bigger than the real epsilon. 
+        public const float EPSILON = float.Epsilon * 100;
+
         public static IArray<IArray<Edge>> Edges(this IGeometry self)
         {
             return self.Elements.Select(Edges);
@@ -206,6 +225,22 @@ namespace Ara3D
         public static IArray<Edge> Edges(this IElement self) 
         {
             return self.Select(i => new Edge(self, i));
+        }
+
+        public static Vector3 MidPoint(this IElement self)
+        {
+            return self.Points().Average();
+        }
+
+        public static IArray<Vector3> EdgeMidPoints(this IElement self)
+        {
+            return self.Edges().Select(e => e.Average());
+        }
+
+        // TODO: this could be broken.
+        public static IArray<Vector3> Points(this IElement self)
+        {
+            return self.Geometry.Vertices.SelectByIndex(self);
         }
 
         public static IEnumerable<int> EnumerateIndices(this IGeometry g)
@@ -236,6 +271,100 @@ namespace Ara3D
         public static PolyFace PolyFace(this IGeometry self, IArray<int> indices)
         {
             return new PolyFace(self, indices);
+        }
+
+        public static int FaceCount(this IGeometry self)
+        {
+            return self.Elements.Count;
+        }
+
+        public static IGeometry ToPolyMesh(this IGeometry self, IEnumerable<IEnumerable<int>> indices)
+        {
+            var verts = self.Vertices;
+            var flatIndices = indices.SelectMany(xs => xs).ToIArray();
+            var faceIndices = indices.Where(xs => xs.Any()).Select(xs => xs.First()).ToIArray();
+            return new BaseMesh(verts, flatIndices, faceIndices);
+        }  
+
+        public static IGeometry MergeCoplanar(this IGeometry self)
+        {
+            if (self.Elements.Count <= 1) return self;
+            var curPoly = new List<int>();
+            var polys = new List<List<int>> { curPoly };
+            var cur = 0;
+            for (var i=1; i < self.Elements.Count; ++i)
+            {
+                if (!self.CanMergeTris(cur, i))
+                {
+                    cur = i;
+                    polys.Add(curPoly = new List<int>());
+                }
+                curPoly.Add(self.Elements[i].ToList());
+            }
+            return self.ToPolyMesh(polys);
+        }        
+
+        public static Vector3 Tangent(this IElement self)
+        {
+            return self.Points()[1] - self.Points()[0];
+        }
+
+        public static Vector3 Binormal(this IElement self)
+        {
+            return self.Points()[2] - self.Points()[0];
+        }
+
+        public static bool Coplanar(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, float epsilon = EPSILON)
+        {
+            // https://en.wikipedia.org/wiki/Coplanarity
+            return Math.Abs(Vector3.Dot(v3 - v1, Vector3.Cross(v2 - v1, v4 - v1))) < epsilon;
+        }
+
+        public static Vector3 Normal(this IElement self)
+        {
+            return Vector3.Normalize(Vector3.Cross(self.Binormal(), self.Tangent()));
+        }
+
+        public static IGeometry ToQuadMesh(Func<Vector2, Vector3> f, int usegs, int vsegs)
+        {
+            var verts = new List<Vector3>();
+            var indices = new List<int>();
+            for (var i = 0; i <= usegs; ++i)
+            {
+                var u = (float)i / usegs;
+                for (var j = 0; j <= vsegs; ++j)
+                {
+                    var v = (float)j / vsegs;
+                    verts.Add(f(new Vector2(u, v)));
+
+                    if (i < usegs && j < vsegs)
+                    {
+                        indices.Add(i * (vsegs + 1) + j);
+                        indices.Add(i * (vsegs + 1) + j + 1);
+                        indices.Add((i + 1) * (vsegs + 1) + j + 1);
+                        indices.Add((i + 1) * (vsegs + 1) + j);
+                    }
+                }
+            }
+            return new QuadMesh(verts.ToIArray(), indices.ToIArray());
+        }
+        public static bool CanMergeTris(this IGeometry self, int a, int b)
+        {
+            var e1 = self.Elements[a];
+            var e2 = self.Elements[b];
+            if (e1.Count != e2.Count && e1.Count != 3) return false;
+            var indices = new[] { e1[0], e1[1], e1[2], e2[0], e2[1], e2[2] }.Distinct().ToIArray();
+            if (indices.Count != 4) return false;
+            var verts = self.Vertices.SelectByIndex(indices);
+            return Coplanar(verts[0], verts[1], verts[2], verts[3]);
+        }
+        public static IEnumerable<Vector3> UsedVertices(this IGeometry self)
+        {
+            return self.Elements.ToEnumerable().SelectMany(es => es.Points().ToEnumerable());
+        }
+        public static IArray<Vector3> FaceMidPoints(this IGeometry self)
+        {
+            return self.Elements.Select(e => e.MidPoint());
         }
     }
 }
